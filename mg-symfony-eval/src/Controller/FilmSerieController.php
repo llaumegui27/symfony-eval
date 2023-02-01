26754469fc7c9097e87bb609f8e0f0602336f0ab
Guillaume Merat <guillaume.merat@campus-la-chataigneraie.org>
2023-02-01 15:24:19 +0100

    first step

---
+++ mg-symfony-eval/src/Controller/FilmSerieController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Film;
+use App\Form\FilmSerieType;
+use App\Repository\FilmRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
+
+class FilmSerieController extends AbstractController
+{
+    #[Route('/filmserie', name: 'app_film_serie')]
+    public function index(): Response
+    {
+        return $this->render('film_serie/index.html.twig', [
+            'controller_name' => 'FilmSerieController',
+        ]);
+    }
+
+    #[Route('/create', name: 'app_add', methods: ['POST'])]
+    public function add(Request $request, ManagerRegistry $doctrine): JsonResponse      // Requete sur Postman
+    {
+        $entityManager = $doctrine->getManager();
+        $data = json_decode($request->getContent(), true);
+
+        $fs = new Film();
+        $fs->setNom($data['nom']);
+        $fs->setSynopsis($data['synopsis']);
+        $fs->setType($data['type']);
+        
+        $fs->setDate($data['date']);
+
+        $entityManager->persist($fs);
+        $entityManager->flush();
+
+        return new JsonResponse('status : 201, success');
+        //return $this->json($fs, Response::HTTP_OK);
+
+    }
+    
+    #[Route('/getall', name: 'app_get_all', methods: ['GET'])]
+    public function getAll(FilmRepository $filmRepository) : Response
+    {
+      
+        $fs = $filmRepository->findAll();
+        $data = [];
+        for($i = 0; $i < count($fs); $i++){
+            $data[] = [
+                'nom' => $fs[$i]->getNom(),
+                'synopsis' => $fs[$i]->getSynopsis(),
+                'type' => $fs[$i]->getType(),
+                'date' => $fs[$i]->getDate(),
+            ];
+        }
+
+        return $this->json($data, Response::HTTP_OK);      
+    
+    }
+
+    #[Route('/get/{id}', name: 'app_get_id', methods: ['GET'])]
+    public function getById(FilmRepository $filmRepository, $id): Response
+    {
+        if (!$filmRepository->find($id)) {
+            throw $this->createNotFoundException(
+                'Aucun(e) film ou série avec l\'id '.$id
+            );
+        }
+  
+        $fs = $filmRepository->find($id);    
+        $data = [];
+
+        $data[] = [
+            'nom' => $fs->getNom(),
+            'synopsis' => $fs->getSynopsis(),
+            'type' => $fs->getType(),
+            'date' => $fs->getDate(),
+        ];
+        
+        return $this->json($data, Response::HTTP_OK);   
+    }
+
+}
---
+++ mg-symfony-eval/src/Entity/Film.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FilmRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FilmRepository::class)]
+class Film
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $synopsis = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $type = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getSynopsis(): ?string
+    {
+        return $this->synopsis;
+    }
+
+    public function setSynopsis(?string $synopsis): self
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
